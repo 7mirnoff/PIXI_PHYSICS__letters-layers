bf87ca0e21d4a71cd50cdcf4044f0e6754366c97
Dmitrey Smirnov <developer@Dmitry.local>
2019-06-06 16:05:24 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -32,47 +32,6 @@ let container = new PIXI.Container()
 app.stage.addChild(container)
 
 const init = () => {
-  // let textHack = new PIXI.Text('NO pain NO gain', {
-  //   fontFamily: 'Alfa Slab One',
-  //   fontSize: 54,
-  //   fill: 0x03aaea,
-  //   align: 'center'
-  // })
-
-  // textHack.position.set(20)
-  // container.addChild(textHack)
-
-  // let textYellow = new PIXI.Text('NO pain NO gain', {
-  //   fontFamily: 'Alfa Slab One',
-  //   fontSize: 54,
-  //   fill: 0xf9ed00,
-  //   align: 'center'
-  // })
-
-  // textYellow.position.set(0)
-  // container.addChild(textYellow)
-
-  // let textRed = new PIXI.Text('NO pain NO gain', {
-  //   fontFamily: 'Alfa Slab One',
-  //   fontSize: 54,
-  //   fill: 0xe80289,
-  //   align: 'center'
-  // })
-
-  // textRed.position.set(10)
-  // container.addChild(textRed)
-
-  // let textBlue = new PIXI.Text('NO pain NO gain', {
-  //   fontFamily: 'Alfa Slab One',
-  //   fontSize: 54,
-  //   fill: 0x03aaea,
-  //   align: 'center'
-  // })
-
-  // textBlue.position.set(20)
-  // textBlue.blendMode = 2
-  // container.addChild(textBlue)
-
   let els = document.querySelectorAll(`.letter`)
   let elsArray = [...els]
 
@@ -80,54 +39,66 @@ const init = () => {
   let letters = []
 
   let layers = new Array(4).fill().map(x => new PIXI.Container())
+  layers[3].blendMode = 2
   layers.forEach(layer => {
     container.addChild(layer)
   })
 
-  // layers[3].blendMode = 2
+  const renderLetter = (text, cords, color, phisicLetterArrays, containers, power, layerStackNumber, fontsize, rotation) => {
+    let containerLetter = new PIXI.Container()
+    let textLetter = new PIXI.Text(text, {
+      fontFamily: 'Alfa Slab One',
+      fontSize: fontsize,
+      fill: color,
+      align: 'center'
+    })
+
+    textLetter.position.x = cords.x
+    textLetter.position.y = cords.y
+
+    if (rotation) {
+      textLetter.rotation = rotation
+    }
+
+    if (layerStackNumber === 3) {
+      textLetter.blendMode = 2
+    }
+
+    containerLetter.addChild(textLetter)
+    layers[layerStackNumber].addChild(containerLetter)
+
+    phisicLetterArrays.push(
+      new PHISIC(textLetter.position.x + cords.width / 2, textLetter.position.y + cords.height / 2, power)
+    )
+
+    containers.push(containerLetter)
+  }
 
   elsArray.forEach(letter => {
     let cordsLetter = letter.getBoundingClientRect()
-
-    const renderLetter = (cords, color, phisicLetterArrays, containers, power, layerStackNumber) => {
-      let containerLetter = new PIXI.Container()
-      let textLetter = new PIXI.Text(letter.innerText, {
-        fontFamily: 'Alfa Slab One',
-        fontSize: 108,
-        fill: color,
-        align: 'center'
-      })
-
-      textLetter.position.x = cords.x
-      textLetter.position.y = cords.y + 350
-
-      if (layerStackNumber === 3) {
-        textLetter.blendMode = 2
-      }
-
-      containerLetter.addChild(textLetter)
-      layers[layerStackNumber].addChild(containerLetter)
-
-      phisicLetterArrays.push(
-        new PHISIC(textLetter.position.x + cordsLetter.width / 2, textLetter.position.y + cordsLetter.height / 2, power)
-      )
-
-      containers.push(containerLetter)
-    }
-
-    renderLetter(cordsLetter, 0x03aaea, phisicLetters, letters, 3, 0)
-    renderLetter(cordsLetter, 0xf9ed00, phisicLetters, letters, 5, 1)
-    renderLetter(cordsLetter, 0xe80289, phisicLetters, letters, 4, 2)
-    renderLetter(cordsLetter, 0x03aaea, phisicLetters, letters, 3, 3)
+    renderLetter(letter.innerText, cordsLetter, 0x03aaea, phisicLetters, letters, 3, 0, 104)
+    renderLetter(letter.innerText, cordsLetter, 0xf9ed00, phisicLetters, letters, 5, 1, 104)
+    renderLetter(letter.innerText, cordsLetter, 0xe80289, phisicLetters, letters, 4, 2, 104)
+    renderLetter(letter.innerText, cordsLetter, 0x03aaea, phisicLetters, letters, 3, 3, 104)
   })
 
-  // let count = 0
-  app.ticker.add(() => {
-    // count++
-    // console.log(phisicLetters)
+  // random smileys
+  for (var i = 100; i > 0; i--) {
+    let coords = {
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+      width: Math.random() * 40 + 10,
+      height: Math.random() * 40 + 10
+    }
+    let randomRotation = 3 * Math.random()
+    renderLetter(':-)))))', coords, 0x03aaea, phisicLetters, letters, 3, 0, coords.width, randomRotation)
+    renderLetter(':-)))))', coords, 0xf9ed00, phisicLetters, letters, 5, 0, coords.width, randomRotation)
+    renderLetter(':-)))))', coords, 0xe80289, phisicLetters, letters, 4, 0, coords.width, randomRotation)
+    renderLetter(':-)))))', coords, 0x03aaea, phisicLetters, letters, 3, 0, coords.width, randomRotation)
+  }
 
+  app.ticker.add(() => {
     let mousposition = app.renderer.plugins.interaction.mouse.global
-    // console.log(mousposition)
 
     phisicLetters.forEach((letter, j) => {
       letter.think(mousposition)
